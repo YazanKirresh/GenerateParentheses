bbb4c6a54e82c18357c67d60730f7e36c737f2ea
Yazan <yazan.kirresh@hotmail.com>
2024-08-31 10:41:10 +0300

    I still do not believe in this Solution

--- src/App.java
+++ src/App.java
@@ -15,18 +15,29 @@ Output: ["()"]
 
 Constraints:
 
+To resolve this we have to follow these steps:
+
  */
 
  import java.util.Stack;
  public class App {
      class Solution {
         static void PrintStack(Stack<Character> s){
-            if (s.empty())
-                return;
-            char x = s.pop();
-            s.pop();
-            PrintStack(s);
-            s.push(x);
+            // If stack is empty 
+            if (s.empty()) 
+                return; 
+               
+            // Extract top of the stack 
+            int x = s.peek();                
+            // Pop the top element 
+            s.pop();                
+            // Print the current top 
+            // of the stack i.e., x 
+            System.out.print(x + " ");               
+            // Proceed to print 
+            // remaining stack 
+            PrintStack(s);                
+            // Push the element back
         }
         public static Stack<Character> generateParenthesis(int n) {
             if (n <= 0) {
@@ -34,10 +45,10 @@ Constraints:
             }
             Stack<Character> stack = new Stack<>();
             stack.push('(');
-            for (int i = 0 ; i < n/2-1; i++){
+            for (int i = 0 ; i < n-1; i++){
                 stack.push('(');
             }
-            for (int i = 0 ; i < n/2; i++){
+            for (int i = 0 ; i < n; i++){
                 stack.push(')');
             }    
             PrintStack(stack) ;
@@ -48,6 +59,6 @@ Constraints:
             System.out.println(AmountOFParentheses);
             generateParenthesis(AmountOFParentheses);
         }
- }
+    }
 }
  
